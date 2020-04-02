adf62b6e7ccfc5d6d7f5f22dff38c1fc45596ed9
Pankaj <pankaj@techtic.in>
2020-04-02 20:21:47 +0530

    Revert "commit changes"
    This reverts commit 73cead770ecebc09875ce6f4616a632d802ab98a.

--- index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import Todolist from './Todolist';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => Todolist);
